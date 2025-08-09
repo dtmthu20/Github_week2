be411d2321f6f26d44a7cacffc94763be362a33f
dtmthu20 <dtmthu@hcmus.edu.vn>
2025-08-09 16:10:39 +0700

    change week2.sh

--- week2.sh
+++ week2.sh
@@ -1,1 +1,39 @@
-Thao tac Github week2
+
+Admin@DESKTOP-FCF5SBE MINGW64 ~ (master)
+$ cd Github_week2/
+
+
+Admin@DESKTOP-FCF5SBE MINGW64 ~/Github_week2 (main)
+$ git branch 
+  dev_v0.1
+* main    
+  test_v1.0
+
+Admin@DESKTOP-FCF5SBE MINGW64 ~/Github_week2 (main)
+$ pwd
+/c/Users/Admin/Github_week2
+
+Admin@DESKTOP-FCF5SBE MINGW64 ~/Github_week2 (main)
+$ git fetch 
+
+Admin@DESKTOP-FCF5SBE MINGW64 ~/Github_week2 (main)
+$ git fetch #download remote repo to local repo 
+
+Admin@DESKTOP-FCF5SBE MINGW64 ~/Github_week2 (main)
+$ git branch #xem cac branch
+  dev_v0.1
+* main
+  test_v1.0
+
+
+Admin@DESKTOP-FCF5SBE MINGW64 ~/Github_week2 (main)
+$ git switch dev_v0.1
+Switched to branch 'dev_v0.1'
+Your branch is up to date with 'origin/dev_v0.1'.
+
+Admin@DESKTOP-FCF5SBE MINGW64 ~/Github_week2 (dev_v0.1)
+$ git switch dev_v0.1 #chuyen giua cac branch
+Already on 'dev_v0.1'
+Your branch is up to date with 'origin/dev_v0.1'.
+
+Admin@DESKTOP-FCF5SBE MINGW64 ~/Github_week2 (dev_v0.1)
